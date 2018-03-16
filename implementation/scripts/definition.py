92fd271845ec0205c9c00c9204f48ea19719ca4e
Uriel González <uriel.gonzalez@firwath.com>
2018-03-16 18:18:03 -0300

    Add mapping between resource elements and archetype nodes
    Maps are formed from paths to resource elements value and its ADL paths in its equivalent archetypes.

--- implementation/scripts/definition.py
+++ implementation/scripts/definition.py
@@ -1,10 +1,15 @@
 import json
 
 identifier = 0
+path = []
+map = []
+mapping = []
 
 def element_object_node(element_obj, terminology, terms):
     """Generate object constraint from element primitive type definition"""
     global identifier
+    global path
+    global map
     term = element_obj['id']
     if 'min' in element_obj.keys():
         minimum = element_obj['min']
@@ -18,8 +23,27 @@ def element_object_node(element_obj, terminology, terms):
         object_constraint = "ELEMENT[id" + str(identifier) + "] occurrences matches {" + str(minimum) + ".." + str(maximum) + "} matches { value matches { " + primitive_type +"[id" + str(identifier + 1) + "] } }"
     else:
         object_constraint = "ELEMENT[id" + str(identifier) + "] matches { value matches { " + primitive_type +"[id" + str(identifier + 1) + "] } }"
+    path.append(identifier + 1)
+    map.append(term)
+    add_map()
+    path.pop()
+    map.pop()
     identifier = identifier + 2
     return object_constraint
+
+def add_map():
+    global path
+    global map
+    global maps
+    a = ""
+    b = ""
+    for segment in path[:len(path)-1]:
+        a = a + "/items[" + str(segment) + "]"
+    a = a + "/value[" + str(path[-1]) + "]"
+    for segment in map[:len(map)-1]:
+        b = b + segment + "."
+    b = b + map[-1]
+    mapping.append({"resoureElementPath" : b, "ADLPath" : a})
 
 def is_primitive(element_obj):
     """Check if an element type definition is primitive"""
@@ -31,6 +55,8 @@ def is_primitive(element_obj):
 
 def cluster_object_node(element_obj, object_constraint, terminology, terms):
     global identifier
+    global path
+    global maps
     """Generate object constraint from complex primitive type definition"""
     if is_primitive(element_obj) == False:
         term = element_obj['id']
@@ -45,9 +71,17 @@ def cluster_object_node(element_obj, object_constraint, terminology, terms):
         else:
             object_constraint = object_constraint + " CLUSTER[id" + str(identifier) + "] matches { items matches {"
         identifier = identifier + 1
+        path.append(identifier)
+        map.append(term)
         for element in element_obj['CML']:
             object_constraint = cluster_object_node(element, object_constraint, terminology, terms)
+            path.pop()
+            path.append(identifier)
+            map.pop()
+            map.append(term)
         object_constraint = object_constraint + " } }"
+        path.pop()
+        map.pop()
     else:
         object_constraint = object_constraint + " " + element_object_node(element_obj, terminology, terms)
     return object_constraint
@@ -74,7 +108,8 @@ if __name__ == '__main__':
     schema_string = schema_source_file.read()
     schema_source_file.close()
     adl_file_name = schema_source.split('\\')[-1]
-    adl_name = adl_file_name.split('.')[0] + '.adls'
+    resource_name = adl_file_name.split('.')[0]
+    adl_name = resource_name + '.adls'
     adl_file = open(output + adl_name,'w')
     adl = 'archetype (adl_version=2.0.6; rm_release=1.0.3)\n'
     adl = adl + 'openehr-ehr-CLUSTER.' + schema_source.split('.')[0] + '-fhir.v0.0.1\n'
@@ -90,3 +125,6 @@ if __name__ == '__main__':
     adl = adl + 'terminology\n' + binding(terminology) + '\n' + binding_terms(terms)
     adl_file.write(adl)
     adl_file.close()
+    map_file = open(output + resource_name + '.mappings.json', 'w')
+    map_file.write(json.dumps(mapping,indent=1))
+    map_file.close()
